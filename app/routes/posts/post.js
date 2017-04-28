6aa76bf1f4c0d710947e74bffe30a329dcb36dfd
Ian Irving <ian@falsepositives.com>
2017-04-28 16:55:39 -0400

    refine model to check local store first

--- app/routes/posts/post.js
+++ app/routes/posts/post.js
@@ -1,19 +1,25 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  store: Ember.inject.service(),
 
   /*
     param is the titleURL of post
   */
   model: function (param) {
-    // if store.post is
     const titleURL = param.titleURL
-    return this.get('store').query('post', {
-      titleURL: titleURL
-    }).then(function (posts) {
+    const store = this.get('store')
+      // peek into local store and see is titleURL exists there
+    let posts = store.peekAll('post')
+    if (Ember.isPresent(posts)) {
       return posts.filterBy('titleURL', titleURL).get('firstObject')
-    })
-
+    } else { // else hit the store and see if turrleURL exists there
+      return store.query('post', {
+        titleURL: titleURL
+      }).then(function (posts) {
+        return posts.filterBy('titleURL', titleURL).get('firstObject')
+      })
+    }
   },
 
   afterModel(model) {
